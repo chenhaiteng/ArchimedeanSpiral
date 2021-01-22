1fbacb47bf8f9879e7c1b7cef6aadbeae2fe5f3e
Chen-Hai Teng <chenhai.teng@positivegrid.com>
2021-01-22 17:05:05 +0800

    1. Implement CGAngle, CGPolarPoint, and ArchimedeanSpiral 2. Add test for CGAngle

--- Sources/ArchimedeanSpiral/ArchimedeanSpiral.swift
+++ Sources/ArchimedeanSpiral/ArchimedeanSpiral.swift
@@ -1,3 +1,93 @@
+//
+//  ArchimedeanSpiral.swift
+//  Hundred-Syllable Mantra WatchKit Extension
+//
+//  Created by Chen Hai Teng on 1/22/21.
+//  Copyright © 2021 Chen-Hai Teng. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct CGAngle {
+    public var radians: CGFloat
+    
+    public var degrees: CGFloat {
+        radians*180/CGFloat.pi
+    }
+
+    @inlinable public init() {
+        radians = 0
+    }
+
+    @inlinable public init<T: BinaryFloatingPoint>(radians: T) {
+        self.radians = CGFloat(radians)
+    }
+    
+    @inlinable public init<T: BinaryFloatingPoint>(degrees: T) {
+        self.radians = CGFloat(degrees)*CGFloat.pi/180
+    }
+    
+    @inlinable public static func radians<T: BinaryFloatingPoint>(_ radians: T) -> CGAngle {
+        CGAngle(radians: radians)
+    }
+    
+    @inlinable public static func degrees<T: BinaryFloatingPoint>(_ degrees: T) -> CGAngle {
+        CGAngle(degrees: degrees)
+    }
+    
+    public static func +(lhs:CGAngle, rhs: CGAngle) -> CGAngle {
+        CGAngle(radians: lhs.radians + rhs.radians)
+    }
+    
+    public static func +=(lhs: inout CGAngle, rhs: CGAngle) {
+        lhs.radians += rhs.radians
+    }
+}
+
+struct CGPolarPoint {
+    var radius: CGFloat
+    var cgangle: CGAngle
+}
+
+extension CGPolarPoint {
+    var point: CGPoint {
+        CGPoint(x: radius*cos(cgangle.radians), y: radius*sin(cgangle.radians))
+    }
+}
+
 struct ArchimedeanSpiral {
-    var text = "Hello, World!"
+    var innerRadius: CGFloat
+    var radiusSpacing: CGFloat
+    var spacing: CGFloat
+    
+    func equidistantPoints(start: CGAngle, num: Int) -> [CGPolarPoint] {
+        guard num > 0 else {
+            return []
+        }
+        var angle = start
+        let a = innerRadius
+        let b = 0.5*radiusSpacing/CGFloat.pi
+        var radius = a + b*angle.radians
+        var points: [CGPolarPoint] = [CGPolarPoint(radius: radius, cgangle: start)]
+        for _ in 1..<num {
+            let delta = approxRadian(radius: radius)
+            angle += delta
+            radius = a + b*angle.radians
+            points.append(CGPolarPoint(radius: radius, cgangle: angle))
+        }
+        return points
+    }
+    
+    // Law of consine
+    // d^2 = r1^2 + r2^2 - 2*r1*r2*cosθ
+    // cosθ = (r1^2 + r2^2 - d^2)/2r1*r2
+    // since r1 ≅ r2, use r1 replace r2
+    // cosθ = (2r1^2 - d^2)/2r1^2 = 1 - 0.5*(d/r1)^2
+    // θ = acos(1 - 0.5*(d/r1)^2)
+    private func approxRadian(radius: CGFloat) -> CGAngle {
+        let d = self.spacing
+        let θ = acos(1.0 - pow(d/radius, 2)*0.5)
+        return CGAngle.radians(θ)
+    }
 }
--- Tests/ArchimedeanSpiralTests/ArchimedeanSpiralTests.swift
+++ Tests/ArchimedeanSpiralTests/ArchimedeanSpiralTests.swift
@@ -2,14 +2,31 @@ import XCTest
 @testable import ArchimedeanSpiral
 
 final class ArchimedeanSpiralTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ArchimedeanSpiral().text, "Hello, World!")
+    func testCGAngle() {
+        let defaultAngle = CGAngle()
+        XCTAssertEqual(defaultAngle.degrees, 0)
+        XCTAssertEqual(defaultAngle.radians, 0)
+        
+        let rightAngleRadians = CGAngle(radians: CGFloat.pi/2)
+        let rightAngleRadiansD = CGAngle(radians: Double.pi/2)
+        XCTAssertEqual(rightAngleRadians.degrees, 90.0)
+        XCTAssertEqual(rightAngleRadiansD.degrees, 90.0)
+        
+        
+        let rightAngle = CGAngle(degrees: CGFloat(90))
+        let rightAngleD = CGAngle(degrees: 90.0)
+        XCTAssertEqual(rightAngle.radians, CGFloat.pi/2)
+        XCTAssertEqual(rightAngleD.radians, CGFloat.pi/2)
     }
+    
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(ArchimedeanSpiral().text, "Hello, World!")
+//    }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testCGAngle", testCGAngle),
     ]
 }
