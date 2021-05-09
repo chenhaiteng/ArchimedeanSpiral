74cf5872637926d67ac2d749be538f70537781f6
Chen-Hai Teng <chenhai.teng@positivegrid.com>
2021-05-09 14:14:34 +0800

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ArchimedeanSpiral",
-            dependencies: []),
+            dependencies: ["CoreGraphicsExtension"]),
         .testTarget(
             name: "ArchimedeanSpiralTests",
             dependencies: ["ArchimedeanSpiral", "CoreGraphicsExtension"]),
