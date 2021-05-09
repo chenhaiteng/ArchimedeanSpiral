4487a7f66992638e824b8f27919298e7da9e98bd
Chen-Hai Teng <chenhai.teng@positivegrid.com>
2021-05-09 15:39:21 +0800

    To avoid initial radius too small

--- Sources/ArchimedeanSpiral/ArchimedeanSpiral.swift
+++ Sources/ArchimedeanSpiral/ArchimedeanSpiral.swift
@@ -29,6 +29,13 @@ public struct ArchimedeanSpiral {
         let a = innerRadius
         let b = 0.5*radiusSpacing/CGFloat.pi
         var radius = a + b*angle.radians
+        
+        // Note: the range of acos should be -1...1
+        // It means 1.0 - pow(d/radius, 2)*0.5 should be in range -1...1
+        // This statements ensure the initial radius located in the range.
+        if radius*2.0 < self.radiusSpacing {
+            radius = self.radiusSpacing/2.0
+        }
         var points: [CGPolarPoint] = [CGPolarPoint(radius: radius, angle: start)]
         for _ in 1..<num {
             let delta = approxRadian(radius: radius)
@@ -45,6 +52,7 @@ public struct ArchimedeanSpiral {
     // since r1 ≅ r2, use r1 replace r2
     // cosθ = (2r1^2 - d^2)/2r1^2 = 1 - 0.5*(d/r1)^2
     // θ = acos(1 - 0.5*(d/r1)^2)
+    
     private func approxRadian(radius: CGFloat) -> CGAngle {
         let d = self.spacing
         let θ = acos(1.0 - pow(d/radius, 2)*0.5)
